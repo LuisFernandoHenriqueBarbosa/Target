9b6562ce58a4ab1d67c9d96e02add5ef98d13eee
Luis Fernando <luisfernandohenriquebarbosa18@hotmail.com>
2024-09-23 15:43:44 -0300

    primeiro exercicio

---
+++ exercicio1.java
@@ -0,0 +1,12 @@
+public class exercicio1{
+    public static void main(String[] args) {
+        int indice = 13, soma = 0, k=0;
+
+        while(k < indice){
+            k= k +1;
+            soma = soma + k;
+        }
+
+        System.out.println("O valor da variável SOMA é: " + soma);
+    }
+}
